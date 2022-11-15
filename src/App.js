e3b1da41e1891dff35738da6ce82a3b6bf5ed638
Natalya <natashanatalia883@gmail.com>
2022-11-15 11:30:08 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather React App</header>
-      <div class="footer">
-        <a
-          href="https://github.com/NatKalyna/example-weather-react-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Open-source code
-        </a>{" "}
-        by Natalia Kalenska 💙💛
+      <div className="container">
+        <header className="App-header">Weather React App</header>
+        <div class="footer">
+          <a
+            href="https://github.com/NatKalyna/example-weather-react-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Natalia Kalenska 💙💛
+        </div>
       </div>
     </div>
   );
