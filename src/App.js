0e0b633408a13fcb126730f04fd63af1a6f070c2
Natalya <natashanatalia883@gmail.com>
2022-11-15 10:45:48 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Weather React App
-        <p>This page was built by Natalia Kalenska</p>
-      </header>
+      <header className="App-header">Weather React App</header>
+      <div class="footer">
+        <a
+          href="https://github.com/NatKalyna/example-weather-react-app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Natalia Kalenska 💙💛
+      </div>
     </div>
   );
 }
