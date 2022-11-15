d920a837286eccdb69d2f57b6fad3175cbd68597
Natalya <natashanatalia883@gmail.com>
2022-11-15 14:36:36 +0200

    Created the Weather.css file

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-4">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-90"
+            />
           </div>
         </div>
       </form>
@@ -23,14 +28,18 @@ export default function Weather() {
         <li>Tuesday 11:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          3℃
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+            />
+            <span className="temperature">3</span>
+            <span className="unit">℃</span>
+          </div>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
