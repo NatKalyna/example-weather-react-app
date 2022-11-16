e605e719e2481362d49d64ea92f2019e6469dec1
Natalya <natashanatalia883@gmail.com>
2022-11-16 14:59:05 +0200

    Created the FormattedDate file

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather() {
@@ -14,7 +15,7 @@ export default function Weather() {
       description: response.data.condition.description,
       humidity: response.data.temperature.humidity,
       feelslike: response.data.temperature.feels_like,
-      date: "Tuesday 11:00",
+      date: new Date(response.data.time * 1000),
       iconUrl:
         "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
     });
@@ -44,7 +45,9 @@ export default function Weather() {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
